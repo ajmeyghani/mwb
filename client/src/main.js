94cde1de4ccc10b296be99b672135bf63803853c
st32lth <st32lth@gmail.com>
2015-12-01 02:45:20 -0500

    add some basic example

--- client/src/main.js
+++ client/src/main.js
@@ -1,30 +1,52 @@
 /*\
- * Load the css
+ * Load some css
 \*/
 require('main.css');
-/*\
- * Load an icon
-\*/
-let icon = require('icon.png');
+
 /*\
  * Set the angular module.
 \*/
 const boiler = angular.module('mymodulename', []);
 
+/*\
+ * A basic contorller
+\*/
 boiler.controller('AppCtrl', function () {
   this.name = `hello boiler`;
 });
 
+/*\                                    /*\
+ * -- Interesting stuff starts here --  *
+\*/                                    /*\
+
+/*\
+ * Loading an asset (png image)
+\*/
+const icon = require('icon.png');
+/*\
+ * Using the png inside a directive
+\*/
 boiler.directive('myimage', function () {
   return {
     restrict: 'E',
-    template: `<image src=${icon}></div>`
+    template: `<div><image src=${icon}></div>`
   };
 });
 
+/*\
+ * Loading an html template
+\*/
+const helloTpl = require('hello.tpl.html');
+/*\
+ * Using ES6 Syntax
+ * Also, using the template (helloTpl) loaded by webpack inside a directive
+\*/
 boiler.directive('hello', () => ({
   restrict: 'E',
-  template: require('hello.tpl.html')
+  template: helloTpl
 }));
-//
+/*\
+ * Export the module at the end
+ * Important to use `module.exports` explicitly
+\*/
 module.exports = boiler;
