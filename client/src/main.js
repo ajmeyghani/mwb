c615e39a529bf950e7b717c080b4650cd8f58d24
st32lth <st32lth@gmail.com>
2015-12-02 13:13:07 -0500

    cleanup

--- client/src/main.js
+++ client/src/main.js
@@ -1,22 +1,7 @@
-require('main.css');
+require('main.css')
 const app = {
-  name: 'boilerplate'
-};
-
-if (IS_DEV) {
-  const angular = require('angular');
+  name: 'mwb'
 }
-const mymodule = angular.module('app', []);
 
-mymodule.directive('boiler', function () {
-  return {
-    restrict: 'E',
-    template: require('tpl.html')
-  };
-});
-
-angular.element(document).ready(function () {
-  angular.bootstrap(document.getElementsByTagName('body')[0], ['app']);
-});
-
-module.exports = app;
+console.log(app.name)
+module.exports = app
